8f006f6bdecbc1a1f63656b7c3727d0cc8c097e4
John Damage <64185976+johndamage@users.noreply.github.com>
2020-06-18 21:56:27 +0800

    Replace old js

--- js/programming-button.js
+++ js/programming-button.js
@@ -1,6 +1,6 @@
 function firstMove() {
   var elem = document.getElementById("myBar");
-  var width = 20;
+  var width = 0;
   var id = setInterval(frame, 10);
   function frame() {
     if (width >= 80) {
@@ -15,10 +15,55 @@ function firstMove() {
 
 function secondMove() {
   var elem = document.getElementById("mySecondBar");
-  var width = 20;
+  var width = 0;
   var id = setInterval(frame, 10);
   function frame() {
-    if (width >= 30) {
+    if (width >= 40) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+// update-01 and all var-width set to 0
+function thirdMove() {
+  var elem = document.getElementById("myThirdBar");
+  var width = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 20) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+
+function fourthMove() {
+  var elem = document.getElementById("myFourthBar");
+  var width = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 20) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+
+function fifthMove() {
+  var elem = document.getElementById("myFifthBar");
+  var width = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 20) {
       clearInterval(id);
     } else {
       width++;
